cd84c1c2311ad70fc900515be7cad679203bbebb
Ashen Dulmina <ashendulminadodangoda@gmail.com>
2024-06-29 19:33:09 +0530

    Added encrypt text frame needs few additional steps to make it user friendly (done for the day)

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ ctk.set_default_color_theme("themes/violet.json") #set color scheme to violet (C
 root = ctk.CTk() #defines root
 root.resizable(False, False) #disables resizing
 root.title("KeeperS | Encryptor And Decryptor 1.0v") #sets title
-root.geometry("500x320") #sets window size
+root.geometry("550x320") #sets window size
 root.iconbitmap("icons/app.ico") #sets app icon
 
 #functions
@@ -186,7 +186,7 @@ creditz_btn_sidebar = ctk.CTkButton(
 	font=("Arial", 10),
 	fg_color="transparent",
 	anchor="w",
-	command=lambda :webbrowser.open_new_tab("https://github.com/Ashen-Dulmina") #takes users to my github page
+	command=lambda :webbrowser.open_new_tab("https://github.com/Ashen-Dulmina") #takes users to my github page  # type: ignore
 )
 
 #the below part assigns the buttons in their corresponding place
@@ -204,16 +204,41 @@ creditz_btn_sidebar.grid(row=8, column=0, sticky="ew")
 
 #key generator framee
 key_gen_frame = ctk.CTkFrame(root,  corner_radius=0, fg_color="transparent") #makes the frame
-lable_one = ctk.CTkLabel(key_gen_frame, text="Generate An Encryption Key", font=("Consolas bold", 18)) #make a text label
-generate_key_button = ctk.CTkButton(key_gen_frame, height=30, width=200, text="Generate A New Key") #make a button
-lable_two = ctk.CTkLabel(key_gen_frame, text="Existing Encryption Keys", font=("Consolas bold", 18)) #make another text label
+lable_one = ctk.CTkLabel(key_gen_frame, text="Generate An Encryption Key", font=("Consolas bold", 18)) #make a text label # under:key_gen_frame
+generate_key_button = ctk.CTkButton(key_gen_frame, height=30, width=200, text="Generate A New Key") #make a button # under:key_gen_frame
+lable_two = ctk.CTkLabel(key_gen_frame, text="Existing Encryption Keys", font=("Consolas bold", 18)) #make another text label # under:key_gen_frame
 #make an embedded json list and a table to list out keys
 
 
-key_gen_frame.grid(row=0, column=1, sticky="nsew")
-lable_one.pack(padx=10, pady=5, anchor=ctk.W)
-generate_key_button.pack(padx=60, pady=8)
-lable_two.pack(padx=10, pady=(17,5), anchor=ctk.W)
+#key_gen_frame.grid(row=0, column=1, sticky="nsew") #align key_gen_frame
+lable_one.pack(padx=10, pady=5, anchor=ctk.W) #pack label one
+generate_key_button.pack(padx=60, pady=8) #pack the button
+lable_two.pack(padx=10, pady=(17,5), anchor=ctk.W) #pack label two
+
+#encrypt text frem
+encrypt_text_frame = ctk.CTkFrame(root, corner_radius=0, fg_color="transparent") #makes a frame
+lable_three = ctk.CTkLabel(encrypt_text_frame, text="Encrypt Text", font=("Consolas bold", 18)) #make a text label # under: encrypt_text_frame
+label_four = ctk.CTkLabel(encrypt_text_frame, text="Select Key :-", font=("Arial", 12)) #make a text label # under: encrypt_text_frame
+keys_path_enc_txt_entry = ctk.CTkEntry(encrypt_text_frame, width=300, height=30, placeholder_text="Key File Path") #key file entry
+browse_icon = ctk.CTkImage(Image.open("icons/browse.png")) #browse files icon
+browse_key_enc_txt_button = ctk.CTkButton(encrypt_text_frame, width=30, height=30, image=browse_icon, text="") # add the browse files button
+label_five = ctk.CTkLabel(encrypt_text_frame, text="Text To Encrypt :-", font=("Arial", 12)) #make a text label
+text_to_encrypt_textbox = ctk.CTkTextbox(encrypt_text_frame, width=330, height=100, border_width=2, corner_radius=10) #make a textbox
+label_six = ctk.CTkLabel(encrypt_text_frame, text="Encrypted Text :-", font=("Arial", 12)) #make text label
+encrypted_text_textbox = ctk.CTkTextbox(encrypt_text_frame, width=330, height=100, border_width=2, corner_radius=10) #make a textbox
+encrypt_text_button = ctk.CTkButton(encrypt_text_frame, text="Encrypt Text") #make the encrypt button
+
+encrypt_text_frame.grid(row=0, column=1, sticky="nsew") #align encrypt_text_frame
+lable_three.pack(padx=10, pady=5, anchor=ctk.W) #pack label 3
+label_four.pack(padx=10, pady=(10,5), anchor=ctk.W) #pack label 4
+keys_path_enc_txt_entry.pack(padx=10, pady=(0,5)) #pack entry
+browse_key_enc_txt_button.pack() #pack browse button
+label_five.pack(padx=5, pady=5) #pack lable 5
+text_to_encrypt_textbox.pack(padx=5, pady=5) #pack textbox
+label_six.pack(padx=5, pady=5) #pack label 6
+encrypted_text_textbox.pack(padx=5, pady=5) #pack textbox
+encrypt_text_button.pack(padx=5, pady=5) #pack encrypt button
+
 
 # window display
 root.mainloop()
